7519023e2e8c86133f8a710ea51694d85398e280
Rowland Rudolf <6936160+rowlandrudolf@users.noreply.github.com>
2024-04-04 02:30:34 -0400

    updated import paths

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { UserModule } from '@app/user/user.module';
+import { UserModule } from '../user/user.module';
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
